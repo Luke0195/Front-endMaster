a76b0ab8de9e638184e6e1e54d117bdd89accacb
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-01-06 13:44:43 -0300

    feat: done project #04

--- project04/assets/js/script.js
+++ project04/assets/js/script.js
@@ -1,4 +1,13 @@
 window.addEventListener('scroll', () =>{
   const header = document.querySelector('header')
   header.classList.toggle('sticky', window.scrollY > 0)
-})
+});
+
+function toggleMenu(){
+  const menuHamburger = document.querySelector('.menu-toggle');
+  menuHamburger.classList.toggle('active');
+    
+  const navegation = document.querySelector('.navigation')
+  navegation.classList.toggle('active')
+  
+}
