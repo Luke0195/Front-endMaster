cf8f4375f269683c506c78d704d6c5e960701fd7
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-01-08 13:44:06 -0300

    feat: add banner-area

--- project05/assets/js/script.js
+++ project05/assets/js/script.js
@@ -1,8 +1,6 @@
 function menuToggle(){
-  const toggleMenu = document.querySelector('.toggle');
-  const sidebar = document.querySelector('.sidebar');
-  toggleMenu.classList.toggle('active')
-  sidebar.classList.toggle('active')
-  
- 
+  const menu = document.querySelector('.menu');
+  const menuOpenend = document.querySelector('.menu-opened');
+  menu.classList.toggle('active');
+  menuOpenend.classList.toggle('active');
 }
