c7c37a5552d69744d9df858a3cbb999f490337f9
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-02-10 12:18:04 -0300

    feat: add responsivity and add menu interaction

--- project07/assets/js/script.js
+++ project07/assets/js/script.js
@@ -1,3 +1,11 @@
 AOS.init({
  duration: 3000
 })
+
+
+function menuToogle(){ 
+  const menuHamburger = document.querySelector('.nav-content');
+  const menu = document.querySelector('.menu-hamburger');
+  menuHamburger.classList.toggle('nav-content-active')
+  menu.classList.toggle('change-icon')
+}
